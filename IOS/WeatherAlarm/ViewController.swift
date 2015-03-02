2cee5cd0edeefd479bd42719de28a7c7df91f918
natsu1211 <longyutao1211@gmail.com>
2015-03-02 21:16:28 +0900

    update iOS version

---
+++ IOS/WeatherAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAlarm
+//
+//  Created by longyutao on 15-2-28.
+//  Copyright (c) 2015年 LongGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
