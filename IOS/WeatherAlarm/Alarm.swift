5ad86a480f3ab455ef33dca7bc9e043dce1529ba
natsu1211 <longyutao1211@gmail.com>
2015-09-20 14:33:22 +0900

    Add media property

--- IOS/WeatherAlarm/Alarm.swift
+++ IOS/WeatherAlarm/Alarm.swift
@@ -7,23 +7,26 @@
 //
 
 import UIKit
-import CoreLocation
 import MediaPlayer
 
 class Alarm: NSObject {
     
    
-    var title:String
-    var time:String
+    var title: String
+    var timeStr: String
+    var time: NSDate
+    var enabled: Bool
     //var region:CLCircularRegion
-    //var media:MPMediaItem
+    var media:MPMediaItem?
     
-    init( title:String, time:String) {
+    init( title: String, timestr: String, time: NSDate, enabled: Bool, media: MPMediaItem?) {
         self.title = title
        // self.region = region
-       // self.media = media
-        self.time = time
-     
         
+        self.timeStr = timestr
+        self.time = time
+        self.enabled = enabled
+        self.media = media
+     
     }
 }
