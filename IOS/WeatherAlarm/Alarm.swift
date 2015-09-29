37d8962da647c9dc04fd7420a7584078b0be4e5a
natsu1211 <longyutao1211@gmail.com>
2015-09-29 17:58:47 +0900

    rename time to date

--- IOS/WeatherAlarm/Alarm.swift
+++ IOS/WeatherAlarm/Alarm.swift
@@ -14,17 +14,17 @@ class Alarm: NSObject {
    
     var title: String
     var timeStr: String
-    var time: NSDate
+    var date: NSDate
     var enabled: Bool
     //var region:CLCircularRegion
     var media:MPMediaItem?
     
-    init( title: String, timestr: String, time: NSDate, enabled: Bool, media: MPMediaItem?) {
+    init( title: String, timestr: String, date: NSDate, enabled: Bool, media: MPMediaItem?) {
         self.title = title
        // self.region = region
         
         self.timeStr = timestr
-        self.time = time
+        self.date = date
         self.enabled = enabled
         self.media = media
      
