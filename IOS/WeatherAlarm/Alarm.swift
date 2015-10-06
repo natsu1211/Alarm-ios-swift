98ae9d05d9d49d14c91a316630f8eb9865cefe1d
natsu1211 <longyutao1211@gmail.com>
2015-10-07 00:53:07 +0900

    Using NSUserDefaults

--- IOS/WeatherAlarm/Alarm.swift
+++ IOS/WeatherAlarm/Alarm.swift
@@ -9,45 +9,77 @@
 import Foundation
 import MediaPlayer
 
-struct Alarm
+class Alarm
 {
+    //using memberwise initializer for struct
     var title: String
     var timeStr: String
     var date: NSDate
     var enabled: Bool
-    //var region:CLCircularRegion
-    var media:MPMediaItem?
+    var UUID: String
+    var mediaID: String
     
-    /*
-    init( title: String, timestr: String, date: NSDate, enabled: Bool, media: MPMediaItem?) {
+    init(title: String, timeStr: String, date: NSDate, enabled: Bool,UUID: String, mediaID: String)
+    {
         self.title = title
-       // self.region = region
-        
-        self.timeStr = timestr
+        self.timeStr = timeStr
         self.date = date
         self.enabled = enabled
-        self.media = media
-     
+        self.UUID = UUID
+        self.mediaID = mediaID
     }
-    */
+
 }
 
-//singleton
-class Alarms
+//singleton, for-in loop supporting
+class Alarms: SequenceType
 {
-    private var alarms = [Alarm]()
-    class var sharedInstance: Alarms
+    private let ud:NSUserDefaults
+    private let alarmKey:String
+    private var alarms:[Alarm] = [Alarm]()
+    
+    //ensure can not be instantiated outside
+    private init()
     {
-        struct Singleton
-        {
-            static let instance: Alarms = Alarms()
-        }
-        return Singleton.instance
+        ud = NSUserDefaults.standardUserDefaults()
+        /*for key in ud.dictionaryRepresentation().keys {
+            NSUserDefaults.standardUserDefaults().removeObjectForKey(key.description)
+        }*/
+        alarmKey = "myAlarmKey"
+        alarms = getAllAlarm()
     }
     
+    //above swift 1.2
+    static let sharedInstance = Alarms()
+    
+    //setObject only support "property list objects",so we cannot persist alarms directly
     func append(alarm: Alarm)
     {
         alarms.append(alarm)
+        //ud.setObject(alarms!,forKey: alarmKey)
+        var alarmDict = ud.dictionaryForKey(alarmKey) ?? [:]
+        alarmDict[alarm.UUID] = ["title": alarm.title, "timeStr": alarm.timeStr, "date": alarm.date, "enabled": alarm.enabled, "UUID": alarm.UUID, "mediaID": alarm.mediaID]
+        ud.setObject(alarmDict, forKey: alarmKey)
+        ud.synchronize()
+    }
+    
+    //helper, convert dictionary to [Alarm]
+    //better if we can get the property name as a string, but Swift does not have any reflection feature now...
+    private func getAllAlarm() -> [Alarm]
+    {
+        var alarmDict = NSUserDefaults.standardUserDefaults().dictionaryForKey(alarmKey)
+        if alarmDict != nil
+        {
+            let items = Array(alarmDict!.values)
+            return (items as! Array<Dictionary<String, AnyObject>>).map(){item in Alarm(title: item["title"] as! String, timeStr: item["timeStr"] as! String, date: item["date"] as! NSDate, enabled: item["enabled"] as! Bool, UUID: item["UUID"] as! String, mediaID: item["mediaID"] as! String)}
+            
+        }
+        else
+        {
+            return [Alarm]()
+        }
+        
+        
     }
     
     var count:Int
@@ -55,7 +87,64 @@ class Alarms
         return alarms.count
     }
     
-    subscript(index: Int) -> Alarm{
-        return alarms[index]
+    func removeAtIndex(index: Int)
+    {
+        alarms.removeAtIndex(index)
     }
+    
+    subscript(index: Int) -> Alarm{
+        get{
+            assert((index < alarms.count && index >= 0), "Index out of range")
+            return alarms[index]
+        }
+        set{
+            assert((index < alarms.count && index >= 0), "Index out of range")
+            alarms[index] = newValue
+        }
+
+    }
+    //helpers for alarm edit. maybe not a good design
+    func titleAtIndex(index: Int) -> String
+    {
+        return alarms[index].title
+    }
+    
+    func timeStrAtIndex(index: Int) -> String
+    {
+        return alarms[index].timeStr
+    }
+    
+    func dateAtIndex(index: Int) -> NSDate
+    {
+        return alarms[index].date
+    }
+    
+    func UUIDAtIndex(index: Int) -> String
+    {
+        return alarms[index].UUID
+    }
+    
+    func enabledAtIndex(index: Int) -> Bool
+    {
+        return alarms[index].enabled
+    }
+    
+    func mediaIDAtIndex(index: Int) -> String
+    {
+        return alarms[index].mediaID
+    }
+    
+    //SequenceType Protocol
+    private var currentIndex = 0
+    func generate() -> GeneratorOf<Alarm> {
+        let next: () -> Alarm? = {
+            if self.currentIndex < self.alarms.count{
+                return self.alarms[self.currentIndex++]
+            }
+            return nil
+        }
+        return GeneratorOf<Alarm>(next)
+    }
+    
+    
 }
