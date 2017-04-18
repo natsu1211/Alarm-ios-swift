6b3214e42257d994c80637095443958b3469a930
natsu1211 <longyutao1211@gmail.com>
2017-04-19 01:33:59 +0900

    add NotificationName extension

---
+++ IOS/Alarm/NotificationName+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName+Extension.swift
+//  Alarm-ios-swift
+//
+//  Created by natsu1211 on 2017/04/18.
+//  Copyright © 2017年 LongGames. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let AlarmDisableNotification = NSNotification.Name("AlarmDisableNotification")
+}
