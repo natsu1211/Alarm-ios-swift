169d361796e542ecb003616f66badf25bda0e1c3
natsu1211 <longyutao1211@gmail.com>
2023-09-14 06:31:21 +0900

    make Store holds Alarms instance

--- IOS/Alarm/Store.swift
+++ IOS/Alarm/Store.swift
@@ -1,20 +1,20 @@
 import Foundation
 
 final class Store {
+    let alarms: Alarms = load()
     // singleton
     static let shared = Store()
     static let changedNotification = Notification.Name("StoreChanged")
-    private let userDefault = UserDefaults.standard
     
     func save(_ data: Alarms, notifying: Alarm?, userInfo: [AnyHashable: Any]) {
         if let jsonData = try? JSONEncoder().encode(data) {
-            userDefault.set(jsonData, forKey: .UserDefaultsKey)
+            UserDefaults.standard.set(jsonData, forKey: .UserDefaultsKey)
         }
         NotificationCenter.default.post(name: Store.changedNotification, object: notifying, userInfo: userInfo)
     }
 
-    func load() -> Alarms{
-        if let data = userDefault.data(forKey: .UserDefaultsKey) {
+    static func load() -> Alarms{
+        if let data = UserDefaults.standard.data(forKey: .UserDefaultsKey) {
             if let alarms = try? JSONDecoder().decode(Alarms.self, from: data) {
                 return alarms
             }
@@ -23,7 +23,7 @@ final class Store {
     }
     
     func clear() {
-        for key in userDefault.dictionaryRepresentation().keys {
+        for key in UserDefaults.standard.dictionaryRepresentation().keys {
             UserDefaults.standard.removeObject(forKey: key.description)
         }
     }
